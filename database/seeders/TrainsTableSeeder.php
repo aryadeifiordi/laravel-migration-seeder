2c606a1f397d331d7815e0763d5944c487ee7f95
aryadeifiordi <arya.93.ar@gmail.com>
2024-05-13 12:14:18 +0200

    seeder done

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -2,16 +2,23 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Train; 
 
 class TrainsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        $trains = [
+            ['azienda' => 'Trenitalia', 'stazione_partenza' => 'Roma', 'stazione_arrivo' => 'Firenze', 'orario_partenza' => '08:00', 'orario_arrivo' => '11:30', 'codice_treno' => '77777', 'numero_carrozze' => 8],
+            ['azienda' => 'Italotreni', 'stazione_partenza' => 'Firenze', 'stazione_arrivo' => 'Bari', 'orario_partenza' => '09:30', 'orario_arrivo' => '13:15', 'codice_treno' => '78960', 'numero_carrozze' => 4],
+        ];
+
+        foreach ($trains as $train) {
+            Train::create($train);
+        }
     }
 }
